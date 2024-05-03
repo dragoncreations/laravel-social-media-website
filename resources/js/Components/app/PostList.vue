16b9bd8b1b9d98d930fc04367e9f2f2a99caa68f
Darek Dawidowicz <ddaw@wp.pl>
2024-05-03 12:42:52 +0200

    Define website home page

---
+++ resources/js/Components/app/PostList.vue
@@ -0,0 +1,66 @@
+<script setup>
+import PostItem from "@/Components/app/PostItem.vue";
+const post1 = {
+    user: {
+        id: 1,
+        avatar: 'https://randomuser.me/api/portraits/men/79.jpg',
+        name: 'John Smith'
+    },
+    group: null,
+    attachments: [
+        {
+            id: 1,
+            name: 'test.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png'
+        },
+        {
+            id: 2,
+            name: 'test2.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png'
+        },
+        {
+            id: 3,
+            name: 'MyDocument.docx',
+            url: '#',
+            mime: 'application/msword'
+        }
+    ],
+    body: `
+        <p>Lorem ipsum dolor sit amet. Ea ipsam laboriosam recusandae voluptatibus? Animi laborum nam quaerat sequi! Obcaecati quos ratione tempora totam. Aspernatur porro possimus repellat suscipit? Harum incidunt ipsam iste vero? Architecto dolorem magni numquam voluptas? Porro recusandae totam ut veritatis. Dolorum earum laudantium magnam maiores. Non obcaecati sit veritatis voluptatibus.</p>
+        
+        <p>Lorem ipsum dolor sit amet. Ea ipsam laboriosam recusandae voluptatibus? Animi laborum nam quaerat sequi! Obcaecati quos ratione tempora totam. Aspernatur porro possimus repellat suscipit? Harum incidunt ipsam iste vero? Architecto dolorem magni numquam voluptas? Porro recusandae totam ut veritatis. Dolorum earum laudantium magnam maiores. Non obcaecati sit veritatis voluptatibus.</p>
+    `,
+    created_at: '2024-05-02 17:04'
+}
+
+const post2 = {
+    user: {
+        id: 2,
+        avatar: 'https://randomuser.me/api/portraits/men/78.jpg',
+        name: 'James Does'
+    },
+    group: {
+        id: 1,
+        name: 'Laravel Developers'
+    },
+    body: `
+        <p>Lorem ipsum dolor sit amet. Ea ipsam laboriosam recusandae voluptatibus? Animi laborum nam quaerat sequi! Obcaecati quos ratione tempora totam. Aspernatur porro possimus repellat suscipit? Harum incidunt ipsam iste vero? Architecto dolorem magni numquam voluptas? Porro recusandae totam ut veritatis. Dolorum earum laudantium magnam maiores. Non obcaecati sit veritatis voluptatibus.</p>
+        
+        <p>Lorem ipsum dolor sit amet. Ea ipsam laboriosam recusandae voluptatibus? Animi laborum nam quaerat sequi! Obcaecati quos ratione tempora totam. Aspernatur porro possimus repellat suscipit? Harum incidunt ipsam iste vero? Architecto dolorem magni numquam voluptas? Porro recusandae totam ut veritatis. Dolorum earum laudantium magnam maiores. Non obcaecati sit veritatis voluptatibus.</p>
+    `,
+    created_at: '2024-05-02 17:04'
+}
+</script>
+
+<template>
+    <div class="overflow-auto">
+        <PostItem :post="post1" />
+        <PostItem :post="post2" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])
-        ->middleware(['auth', 'verified'])->name('home');
+        ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
