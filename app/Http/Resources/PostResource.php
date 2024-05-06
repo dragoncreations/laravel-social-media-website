6de9f319db063ac9e1a550bd6541ccb78e0e41ae
Darek Dawidowicz <ddaw@wp.pl>
2024-05-06 19:04:01 +0200

    Implement post update

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -18,7 +18,7 @@ class PostResource extends JsonResource
             'id' => $this->id,
             'body' => $this->body,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at,
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'user' => new UserResource($this->user),
             'group' => $this->group,
             'attachments' => $this->attachments,
