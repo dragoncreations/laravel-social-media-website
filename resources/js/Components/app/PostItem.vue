4f83fe998b02c6f4b5b3f218a72c87d5e303633a
Darek Dawidowicz <ddaw@wp.pl>
2024-05-07 05:58:24 +0200

    Implement post delete

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -3,6 +3,7 @@ import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { PencilIcon, TrashIcon, EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import { ref } from "vue";
+import { router } from '@inertiajs/vue3';
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
 
 const props = defineProps({
@@ -18,6 +19,14 @@ function isImage(attachment){
 
 function openEditModal(){
     emit('editClick', props.post)
+}
+
+function deletePost(){
+    if(window.confirm('Are you sure you want to delete this post?')){
+        router.delete(route('post.destroy', props.post), {
+            preserveScroll: true
+        })
+    }
 }
 
 </script>
@@ -67,6 +76,7 @@ function openEditModal(){
                             </MenuItem>
                             <MenuItem v-slot="{ active }">
                                 <button
+                                  @click="deletePost"
                                   :class="[
                                   active ? 'bg-indigo-600 text-white' : 'text-gray-900',
                                   'group flex w-full items-center rounded-md px-2 py-2 text-sm',
