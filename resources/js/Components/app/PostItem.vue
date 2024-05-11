e3eb433da9479b846abc4b5913f63cc30bde96dc
Darek Dawidowicz <ddaw@wp.pl>
2024-05-11 08:01:21 +0200

    Create new PostAttachments component

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -3,17 +3,17 @@ import { ChatBubbleLeftRightIcon, ChatBubbleLeftEllipsisIcon, HandThumbUpIcon, A
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import PostUserHeader from "@/Components/app/PostUserHeader.vue";
 import { router, usePage } from '@inertiajs/vue3';
-import { isImage } from '@/helpers.js'
-import {PaperClipIcon} from "@heroicons/vue/24/solid/index.js";
 import axiosClient from "@/axiosClient.js";
 import InputTextarea from "@/Components/InputTextarea.vue";
 import IndigoButton from '@/Components/app/IndigoButton.vue';
 import ReadMoreReadLess from '@/Components/app/ReadMoreReadLess.vue';
 import EditDeleteDropdown from '@/Components/app/EditDeleteDropdown.vue';
+import PostAttachments from '@/Components/app/PostAttachments.vue';
 import { ref } from 'vue'
 
 const authUser = usePage().props.auth.user;
 const editingComment = ref(null);
+
 const props = defineProps({
     post: Object
 })
@@ -114,31 +114,10 @@ function sendCommentReaction(comment){
         <div class="grid gap-3 mb-3" :class="[
             post.attachments.length === 1 ? 'grid-cols-1' : 'grid-cols-2'
         ]">
-            <template v-for="(attachment, ind) of post.attachments.slice(0, 4)">
-
-                <div @click="openAttachment(ind)"
-                  class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
-                    <div v-if="ind === 3 && post.attachments.length > 4" class="absolute left-0 top-0 right-0 bottom-0 z-10 bg-black/60 text-white flex items-center justify-center text-2xl">
-                        +{{ post.attachments.length - 4 }} more
-                    </div>
-                    <!-- Download -->
-                    <a @click.stop :href="route('post.download', attachment)"
-                        class="z-20 opacity-0 group-hover:opacity-100 transition-all w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
-                        <ArrowDownTrayIcon class="w-4 h-4" />
-                    </a>
-                    <!--/ Download -->
-
-                    <img v-if="isImage(attachment)" 
-                        :src="attachment.url" 
-                        class="object-contain aspect-square" />
-                    <div v-else class="flex flex-col justify-center items-center">
-                        <PaperClipIcon class="w-10 h-10 mb-3" />
-                        <small>{{ attachment.name }}</small>
-                    </div>
-                </div>
-            </template>
+            <PostAttachments :attachments="post.attachments" @attachmentClick="openAttachment" />
         </div>
         <Disclosure v-slot="{ open }">
+        <!-- Like & Comment buttons -->
             <div class="flex gap-2">
                 <button
                     @click="sendReaction"
@@ -161,6 +140,7 @@ function sendCommentReaction(comment){
                     Comment
                 </DisclosureButton>
             </div>
+
             <DisclosurePanel class="mt-3">
                 <div class="flex gap-2 mb-3">
                     <a href="javascript:void(0)">
@@ -190,39 +170,47 @@ function sendCommentReaction(comment){
                                     <small class="text-xs text-gray-400">{{ comment.updated_at }}</small>
                                 </div>
                             </div>
-                            <EditDeleteDropdown :user="comment.user" @edit="startCommentEdit(comment)" @delete="deleteComment(comment)" />
+                            <EditDeleteDropdown :user="comment.user" @edit="startCommentEdit(comment)" 
+                                                @delete="deleteComment(comment)" />
                         </div>
                         <div class="pl-12">
                             <div v-if="editingComment && editingComment.id === comment.id">
-                                <InputTextarea v-model="editingComment.comment" placeholder="Enter your comment here" rows="1" class="max-h-[160px] w-full resize-none"></InputTextarea>
+                                <InputTextarea v-model="editingComment.comment" placeholder="Enter your comment here" 
+                                               rows="1" class="max-h-[160px] w-full resize-none"></InputTextarea>
+
                                 <div class="flex gap-2 justify-end">
-                                    <button @click="editingComment = null" class="rounded-r-none text-indigo-500">cancel</button>
-                                    <IndigoButton @click="updateComment" class="w-[100px]">update</IndigoButton>
+                                    <button @click="editingComment = null" class="rounded-r-none text-indigo-500">cancel
+                                    </button>
+                                    <IndigoButton @click="updateComment" class="w-[100px]">update
+                                    </IndigoButton>
                                 </div>
                             </div>
                             <ReadMoreReadLess v-else :content="comment.comment" content-class="text-sm flex flex-1" />
                             <div class="mt-1 flex gap-2">
                                 <button @click="sendCommentReaction(comment)" 
-                                  class="flex items-center text-xs text-indigo-500 py-0.5 px-1 rounded-lg"
-                                  :class="[
-                                      comment.current_user_has_reaction ? 
-                                        'bg-indigo-50 hover:bg-indigo-100' : 
-                                        'hover:bg-indigo-50'
-                                  ]">
+                                        class="flex items-center text-xs text-indigo-500 py-0.5 px-1 rounded-lg"
+                                        :class="[
+                                            comment.current_user_has_reaction ? 
+                                            'bg-indigo-50 hover:bg-indigo-100' : 
+                                            'hover:bg-indigo-50'
+                                        ]">
                                     <HandThumbUpIcon class="w-3 h-3 mr-1" />
                                     <span class="mr-2">{{ comment.num_of_reactions }}</span>
                                     {{ comment.current_user_has_reaction ? 'unlike' : 'like' }}
                                 </button>
-                                <button class="flex items-center text-xs text-indigo-500 py-0.5 px-1 hover:bg-indigo-100 rounded-lg">
+                                <button 
+                                    class="flex items-center text-xs text-indigo-500 py-0.5 px-1 hover:bg-indigo-100 rounded-lg">
                                     <ChatBubbleLeftEllipsisIcon class="w-3 h-3 mr-1" />
                                     reply
                                 </button>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </DisclosurePanel>
         </Disclosure>
+
     </div>
 </template>
 
