f6a770aacc3cf283c61428ba4170626cc58dd927
Darek Dawidowicz <ddaw@wp.pl>
2024-05-11 19:24:14 +0200

    Create reusable CommentList.vue component for subcomments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,16 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Comment extends Model
 {
     use HasFactory;
     
-    protected $fillable = ['post_id', 'comment', 'user_id'];
+    protected $fillable = ['post_id', 'comment', 'user_id', 'parent_id'];
     
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+    
+    public function comments(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id');
     }
     
     public function post(): BelongsTo {
