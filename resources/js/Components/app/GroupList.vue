38492ab949a5144db62ba14aeb8dc473b6a0a8a0
Darek Dawidowicz <ddaw@wp.pl>
2024-05-18 07:02:49 +0200

    Implement creating group

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/vue";
 import GroupListItems from "@/Components/app/GroupListItems.vue";
+import GroupModal from "@/Components/app/GroupModal.vue";
+import {ref} from "vue";
+
+const showNewGroupModal = ref(false)
+
 </script>
 
 <template>
@@ -10,21 +15,34 @@ import GroupListItems from "@/Components/app/GroupListItems.vue";
                 <DisclosureButton class="w-full">
                     <div class="flex justify-between items-center">
                         <h2 class="text-xl font-bold">My Groups</h2>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 transition-all" :class="open ? 'rotate-90 transform' : ''">
-                            <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-6 h-6 transition-all"
+                             :class="open ? 'rotate-90 transform' : ''">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
                         </svg>
                     </div>
                 </DisclosureButton>
                 <DisclosurePanel>
-                    <GroupListItems />
+                    <button @click="showNewGroupModal = true"
+                            class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2">
+                        new group
+                    </button>
+                    <GroupListItems/>
                 </DisclosurePanel>
             </Disclosure>
         </div>
         <div class="h-full overflow-hidden flex-col hidden lg:flex">
-            <h2 class="text-xl font-bold">My Groups</h2>
-            <GroupListItems />
+            <div class="flex justify-between">
+                <h2 class="text-xl font-bold">My Groups</h2>
+                <button @click="showNewGroupModal = true"
+                        class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2">
+                    new group
+                </button>
+            </div>
+            <GroupListItems/>
         </div>
     </div>
+    <GroupModal v-model="showNewGroupModal"/>
 </template>
 
 <style scoped>
