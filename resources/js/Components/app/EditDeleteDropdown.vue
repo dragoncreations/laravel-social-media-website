b907bfd01d8f1d717a1eb8ef6bdfca7670f788c8
Darek Dawidowicz <ddaw@wp.pl>
2024-05-22 19:41:07 +0200

    Create dedicated post page

--- resources/js/Components/app/EditDeleteDropdown.vue
+++ resources/js/Components/app/EditDeleteDropdown.vue
@@ -1,9 +1,10 @@
 <script setup>
 
-import { usePage } from '@inertiajs/vue3';
-import { PencilIcon, TrashIcon, EllipsisVerticalIcon } from '@heroicons/vue/24/solid'
+import { EllipsisVerticalIcon, PencilIcon, TrashIcon, EyeIcon } from "@heroicons/vue/20/solid/index.js";
+import { ClipboardIcon } from "@heroicons/vue/24/outline";
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-import {computed} from "vue";
+import { usePage, Link } from "@inertiajs/vue3";
+import { computed } from "vue";
 
 const props = defineProps({
     post: {
@@ -26,11 +27,27 @@ const editAllowed = computed(() => {
 
 const deleteAllowed = computed(() => {
     if (user.value.id === authUser.id) return true;
+
     if (props.post.user.id === authUser.id) return true;
+
     return !props.comment && props.post.group?.role === 'admin';
 })
 
 defineEmits(['edit', 'delete'])
+
+function copyToClipboard() {
+    // Replace 'your-text-to-copy' with the actual text you want to copy
+    const textToCopy = route('post.view', props.post.id);
+    // Create a temporary element to copy the text
+    const tempInput = document.createElement('input');
+    tempInput.value = textToCopy;
+    document.body.appendChild(tempInput);
+    // Select the text in the temporary input
+    tempInput.select();
+    document.execCommand('copy');
+    // Remove the temporary input from the DOM
+    document.body.removeChild(tempInput);
+}
 
 </script>
 
@@ -56,9 +73,38 @@ defineEmits(['edit', 'delete'])
           leave-to-class="transform scale-95 opacity-0"
         >
             <MenuItems
-              class="absolute z-20 right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+              class="absolute z-20 right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
             >
                 <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                        <Link :href="route('post.view', post.id)"
+                              :class="[
+                              active ? 'bg-indigo-500 text-white' : 'text-gray-900',
+                              'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                            ]"
+                        >
+                            <EyeIcon
+                                class="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            Open Post
+                        </Link>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <button
+                            @click="copyToClipboard"
+                            :class="[
+                              active ? 'bg-indigo-500 text-white' : 'text-gray-900',
+                              'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                            ]"
+                        >
+                            <ClipboardIcon
+                                class="mr-2 h-5 w-5"
+                                aria-hidden="true"
+                            />
+                            Copy Post URL
+                        </button>
+                    </MenuItem>
                     <MenuItem v-if="editAllowed" v-slot="{ active }">
                         <button
                           @click="$emit('edit')"
