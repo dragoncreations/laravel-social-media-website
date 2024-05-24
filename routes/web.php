bf76c7425db6e8984de1991a884858a803c60136
Darek Dawidowicz <ddaw@wp.pl>
2024-05-24 16:24:31 +0200

    Implement follow/unfollow on user, send email notifications as well.

--- routes/web.php
+++ routes/web.php
@@ -4,84 +4,107 @@ use App\Http\Controllers\GroupController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', [HomeController::class, 'index'])
-        ->middleware(['auth', 'verified'])->name('dashboard');
+    ->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::get('/u/{user:username}', [ProfileController::class, 'index'])
-        ->name('profile');
+    ->name('profile');
 
 Route::get('/g/{group:slug}', [GroupController::class, 'profile'])
-        ->name('group.profile');
+    ->name('group.profile');
 
 Route::get('/group/approve-invitation/{token}', [GroupController::class, 'approveInvitation'])
     ->name('group.approveInvitation');
 
 Route::middleware('auth')->group(function () {
-    Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
-            ->name('profile.updateImages');
-    
-    Route::post('/group/update-images/{group:slug}', [GroupController::class, 'updateImage'])
-        ->name('group.updateImages');
-    
+
     // Groups
-    Route::post('/group', [GroupController::class, 'store'])
-        ->name('group.create');
-    
-    Route::put('/group/{group:slug}', [GroupController::class, 'update'])
-        ->name('group.update');
-    
-    Route::post('/group/invite/{group:slug}', [GroupController::class, 'inviteUsers'])
-        ->name('group.inviteUsers');
-    
-    Route::post('/group/join/{group:slug}', [GroupController::class, 'join'])
-        ->name('group.join');
-    
-    Route::post('/group/approve-request/{group:slug}', [GroupController::class, 'approveRequest'])
-        ->name('group.approveRequest');
-    
-    Route::delete('/group/remove-user/{group:slug}', [GroupController::class, 'removeUser'])
-        ->name('group.removeUser');
-    
-    Route::post('/group/change-role/{group:slug}', [GroupController::class, 'changeRole'])
-        ->name('group.changeRole');
-    
+    Route::prefix('/group')->group(function () {
+
+        Route::post('/', [GroupController::class, 'store'])
+            ->name('group.create');
+
+        Route::put('/{group:slug}', [GroupController::class, 'update'])
+            ->name('group.update');
+
+        Route::post('/update-images/{group:slug}', [GroupController::class, 'updateImage'])
+            ->name('group.updateImages');
+
+        Route::post('/invite/{group:slug}', [GroupController::class, 'inviteUsers'])
+            ->name('group.inviteUsers');
+
+        Route::post('/join/{group:slug}', [GroupController::class, 'join'])
+            ->name('group.join');
+
+        Route::post('/approve-request/{group:slug}', [GroupController::class, 'approveRequest'])
+            ->name('group.approveRequest');
+
+        Route::delete('/remove-user/{group:slug}', [GroupController::class, 'removeUser'])
+            ->name('group.removeUser');
+
+        Route::post('/change-role/{group:slug}', [GroupController::class, 'changeRole'])
+            ->name('group.changeRole');
+    });
+
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    // Posts
-    Route::get('/post/{post}', [PostController::class, 'view'])
-        ->name('post.view');
+    Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
+        ->name('profile.updateImages');
 
-    Route::post('/post', [PostController::class, 'store'])
+    Route::post('/user/follow/{user}', [UserController::class, 'follow'])->name('user.follow');
+
+    // Posts
+    Route::prefix('/post')->group(function () {
+        Route::get('/{post}', [PostController::class, 'view'])
+            ->name('post.view');
+
+        Route::post('', [PostController::class, 'store'])
             ->name('post.create');
-    
-    Route::put('/post/{post}', [PostController::class, 'update'])
+
+        Route::put('/{post}', [PostController::class, 'update'])
             ->name('post.update');
-    
-    Route::delete('/post/{post}', [PostController::class, 'destroy'])
+
+        Route::delete('/{post}', [PostController::class, 'destroy'])
             ->name('post.destroy');
-    
-    Route::get('/post/download/{attachment}', [PostController::class, 'downloadAttachment'])
+
+        Route::get('/download/{attachment}', [PostController::class, 'downloadAttachment'])
             ->name('post.download');
-   
-    Route::post('/post/{post}/reaction', [PostController::class, 'postReaction'])
+
+        Route::post('/{post}/reaction', [PostController::class, 'postReaction'])
             ->name('post.reaction');
-    
-    Route::post('/post/{post}/comment', [PostController::class, 'createComment'])
+
+        Route::post('/{post}/comment', [PostController::class, 'createComment'])
             ->name('post.comment.create');
-    
+    });
+
     // Comments
     Route::delete('/comment/{comment}', [PostController::class, 'deleteComment'])
-            ->name('comment.delete');
-    
+        ->name('comment.delete');
+
     Route::put('/comment/{comment}', [PostController::class, 'updateComment'])
-            ->name('comment.update');
-    
+        ->name('comment.update');
+
     Route::post('/comment/{comment}/reaction', [PostController::class, 'commentReaction'])
-            ->name('comment.reaction');
+        ->name('comment.reaction');
+
 });
 
 require __DIR__ . '/auth.php';
