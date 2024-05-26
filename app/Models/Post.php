970dac96fd5e5083c2c2fba457424a9c935a1ace
Darek Dawidowicz <ddaw@wp.pl>
2024-05-26 13:57:37 +0200

    Implement functionality to pin/unpin posts on group or user profile pages

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -42,9 +42,9 @@ class Post extends Model
         return $this->hasMany(Comment::class)->latest();
     }
     
-    public static function postsForTimeline($userId): Builder
+    public static function postsForTimeline($userId, $getLatest = true): Builder
     {
-        return Post::query() // SELECT * FROM posts
+        $query = Post::query() // SELECT * FROM posts
         ->withCount('reactions') // SELECT COUNT(*) from reactions
         ->with([
             'comments' => function ($query) {
@@ -53,8 +53,13 @@ class Post extends Model
             },
             'reactions' => function ($query) use ($userId) {
                 $query->where('user_id', $userId); // SELECT * from reactions WHERE user_id = ?
-            }])
-            ->latest();
+            }
+        ]);
+        if ($getLatest) {
+            $query->latest();
+        }
+
+        return $query;
     }
     
     public function isOwner($userId)
